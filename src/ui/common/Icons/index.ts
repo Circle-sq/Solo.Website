29f8c97fab4893f6da82a35a92d2c44b6d5486d2
StanislavCaragacianSM <stanislav.caragacian@softwaremind.com>
2025-09-15 12:18:33 +0300

    SB R6: Update top nav icons

--- src/ui/common/Icons/index.ts
+++ src/ui/common/Icons/index.ts
@@ -1,6 +1,4 @@
-import AzIcon from './AzIcon';
-import BetsIcon from './BetsIcon';
 import CashOutIcon from './CashOutIcon';
 import NotificationsIcon from './NotificationsIcon';
 
-export { AzIcon, BetsIcon, CashOutIcon, NotificationsIcon };
+export { CashOutIcon, NotificationsIcon };
